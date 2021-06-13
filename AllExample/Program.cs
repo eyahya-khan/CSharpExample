32a1fdefb94fe8183bbc21b28d1afbfa4823d508
eyahya-khan <eyahyakhan@gmail.com>
2021-06-13 10:52:27 +0200

    Math Function

--- AllExample/Program.cs
+++ AllExample/Program.cs
@@ -79,9 +79,15 @@ namespace AllExample
             int age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Your age is: " + age);*/
 
+            //######### MATH #########
+            /*Console.WriteLine(Math.Max(5, 10));
+            Console.WriteLine(Math.Min(5, 10));
+            Console.WriteLine(Math.Sqrt(64));
+            Console.WriteLine(Math.Abs(-4.7));
+            Console.WriteLine(Math.Round(9.99));*/
 
             //######### TYPE CASTING #########
-            //######### TYPE CASTING #########
+
             //######### TYPE CASTING #########
             //######### TYPE CASTING #########
 
