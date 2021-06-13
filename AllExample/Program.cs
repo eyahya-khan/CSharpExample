3bc56d0a61b1d76a865ee06c120ddbf318003233
eyahya-khan <eyahyakhan@gmail.com>
2021-06-13 10:31:08 +0200

    variable example

--- AllExample/Program.cs
+++ AllExample/Program.cs
@@ -7,7 +7,42 @@ namespace AllExample
         static void Main(string[] args)
         {
             //basic example
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            //######### VARIABLE #########
+            //create string variable
+            /*string name = "John";
+            Console.WriteLine(name);*/
+
+            //create int variable
+            /*int myNum = 15;
+            Console.WriteLine(myNum);*/
+
+            //create variable assinging value later
+            /*int myNum;
+            myNum = 15;
+            Console.WriteLine(myNum);*/
+
+            //overwrite value
+            /*int myNum = 15;
+            myNum = 20;
+            Console.WriteLine(myNum);*/
+
+            //combine text & variable
+            /*string name = "John";
+             Console.WriteLine("Hello " + name);*/
+
+            //add variable with another variable
+            /*string firstName = "John ";
+            string lastName = "Doe";
+            string fullName = firstName + lastName;
+            Console.WriteLine(fullName);*/
+
+            //declare many variable
+            /*int x = 5, y = 6, z = 50;
+            Console.WriteLine(x + y + z);*/
+
+
         }
     }
 }
