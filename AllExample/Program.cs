b66f34152a3d32821bbc4c67226db4704766c551
eyahya-khan <eyahyakhan@gmail.com>
2021-06-13 11:16:24 +0200

    Condition: if else & switch

--- AllExample/Program.cs
+++ AllExample/Program.cs
@@ -136,8 +136,49 @@ namespace AllExample
             Console.WriteLine(txt);*/
 
             //######### if ... else if ... else #########
+            /*int time = 22;
+            if (time < 10)
+            {
+                Console.WriteLine("Good morning.");
+            }
+            else if (time < 20)
+            {
+                Console.WriteLine("Good day.");
+            }
+            else
+            {
+                Console.WriteLine("Good evening.");
+            }*/
 
-            //######### TYPE CASTING #########
+            //######### SWITCH CASE #########
+            /*int day = 4;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Looking forward to the Weekend.");
+                    break;
+            }*/
 
 
         }
